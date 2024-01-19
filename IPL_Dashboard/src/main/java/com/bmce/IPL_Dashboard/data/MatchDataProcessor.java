fc6790c5e54c5ea7f20727f2d7aa8f7bf3e5260d
Josel Xavier <joselxavior@bmce.ac.in>
2024-01-20 05:26:23 +0530

    Sring batch process :: process class is defined , match input class is defined and returned the Match class(output).

--- IPL_Dashboard/src/main/java/com/bmce/IPL_Dashboard/data/MatchDataProcessor.java
+++ IPL_Dashboard/src/main/java/com/bmce/IPL_Dashboard/data/MatchDataProcessor.java
@@ -39,7 +39,11 @@ public class MatchDataProcessor implements ItemProcessor<MatchInput,Match> {
             match.setTeam2(secondInningsTeam);
             match.setTossWinner(matchInput.getToss_winner());
             match.setTossDecision(matchInput.getToss_decision());
-            match.setMatchWinner(matchInput.());
+            match.setMatchWinner(matchInput.getWinner());
+            match.setResult((matchInput.getResult()));
+            match.setResultMargin(matchInput.getResult_margin());
+            match.setUmpire1(matchInput.getUmpire1());
+            match.setUmpire2(matchInput.getUmpire2());
 
 
             return match;
