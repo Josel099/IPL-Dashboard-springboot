53b9c6a21c7c774df7b9254f078e69f192d25e76
Josel Xavier_mac <joseltech2002@gmail.com>
2024-06-06 23:33:30 +0530

    pageable request done

--- IPL_Dashboard/src/main/java/com/bmce/IPL_Dashboard/controller/TeamController.java
+++ IPL_Dashboard/src/main/java/com/bmce/IPL_Dashboard/controller/TeamController.java
@@ -1,5 +1,9 @@
 package com.bmce.IPL_Dashboard.controller;
 
+
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,10 +25,10 @@ public class TeamController {
 
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
-
+        Pageable pageable = PageRequest.of(0,4);
         Team team = this.teamRepository.findByTeamName(teamName);
-        team.setMatchs(matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName));
-
+        team.setMatchs(matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName,pageable));
+        
         return team;
     }
 
--- IPL_Dashboard/src/main/java/com/bmce/IPL_Dashboard/repository/MatchRepository.java
+++ IPL_Dashboard/src/main/java/com/bmce/IPL_Dashboard/repository/MatchRepository.java
@@ -2,6 +2,7 @@ package com.bmce.IPL_Dashboard.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,5 @@ import com.bmce.IPL_Dashboard.model.Match;
 @Repository
 public interface MatchRepository extends CrudRepository<Match, Long> {
 
-    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1 , String teamName2); 
+    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1 , String teamName2,Pageable pageable); 
 }
