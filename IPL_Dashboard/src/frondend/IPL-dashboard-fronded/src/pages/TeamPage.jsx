9ce287aef1d861dc578d459d4319d0d2dc2bfa7f
Josel Xavier_m <josel.x@livares.com>
2024-09-27 23:06:32 +0530

    use effect add in UI . crossorigin added in the teamController.

--- IPL_Dashboard/src/frondend/IPL-dashboard-fronded/src/pages/TeamPage.jsx
+++ IPL_Dashboard/src/frondend/IPL-dashboard-fronded/src/pages/TeamPage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect ,useState} from 'react'
 import MatchDetailCard from '../components/MatchDetailCard'
 import MatchSmallCard from '../components/MatchSmallCard'
 
 function TeamPage() {
+  const[team,setTeam] = useState();
+
+
+  useEffect(()=>{
+    const fetchMatches = async () => {
+      const response = await fetch('http://localhost:8080/team/Rajasthan Royals')
+      const data = await response.json()
+      setTeam(data); 
+      console.log(data)
+    }
+    fetchMatches()
+  })
+
+
+
     return (
         <div className='TeamPage'>
-         <h1>Team Name</h1>
+         <h1>{team.teamName}</h1>
 <MatchDetailCard/>
 <MatchSmallCard/>
 <MatchSmallCard/>
